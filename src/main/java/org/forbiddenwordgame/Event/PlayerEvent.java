b0ac1eed40c8da99549ba464c838a2b5efe94c5e
Foodust <77597986+Foodust@users.noreply.github.com>
2024-07-02 21:02:31 +0900

    [feat] event

--- src/main/java/org/forbiddenwordgame/Event/PlayerEvent.java
+++ src/main/java/org/forbiddenwordgame/Event/PlayerEvent.java
@@ -1,14 +1,23 @@
 package org.forbiddenwordgame.Event;
 
+import ir.taher7.melodymine.api.events.PlayerChangeTalkEvent;
 import ir.taher7.melodymine.api.events.PlayerStartVoiceEvent;
 import ir.taher7.melodymine.models.MelodyPlayer;
+import ir.taher7.melodymine.models.MelodyTalk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.forbiddenwordgame.ForbiddenWordGame;
 
 public class PlayerEvent implements Listener {
 
+    public PlayerEvent(ForbiddenWordGame plugin) {
+
+    }
+
     @EventHandler
-    public void onPlayerStartVoice(PlayerStartVoiceEvent event) {
-        MelodyPlayer melodyPlayer = event.getMelodyPlayer();
+    public void onPlayerChangeTalk(PlayerChangeTalkEvent event) {
+        MelodyPlayer player = event.getMelodyPlayer();
+        String server = event.getMelodyTalk().getServer();
+        MelodyTalk melodyTalk = event.getMelodyTalk();
     }
 }
